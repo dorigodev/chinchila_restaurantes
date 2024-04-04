52143cac53404ac2f24b8bc2efb8ce6306ed424e
Murilo Dorigo <murillodorigo@gmail.com>
2024-04-04 20:21:46 -0300

    Resolvido Ex do 2 Módulo

--- curso-1/ex-2.py
+++ curso-1/ex-2.py
@@ -9,8 +9,33 @@ idade = int(input("Qual sua idade: "))
 
 if (idade<=12):
     print("Você é criança")
-elif (idade > 13) & (idade < 18):
+elif (idade >= 13) & (idade <= 18):
     print("você é adolecente")
 elif (idade > 18):
     print("você é adulto")
+
+nome_de_usuario_inserido = input("Digite um nome de Usuário\n")
+senha_do_usuario_inserido = input("Digite uma senha\n")
+
+nome_de_usuario = "Ruffus"
+senha = "Pituco22"
+
+if nome_de_usuario_inserido == nome_de_usuario_inserido and senha_do_usuario_inserido == senha:
+    print(f"Bem vindo {nome_de_usuario}")
+else:
+    print("Usuário Inválido")
     
+x = float(input('informe o x\n'))
+y = float(input('informe o y\n'))
+
+
+if x>0 and y>0:
+    print("Primeiro Quadrante")
+elif x<0 and y>0:
+    print("Segundo Quadrante")
+elif x<0 and x<0:
+    print("Terceiro Quadrante")
+elif x<0 and y<0:
+    print("Quarto Quadrante")
+else:
+    print("Marco Zero")
